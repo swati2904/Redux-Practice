af75fae30ed87bd51ee107fa51bf026f73c8636c
swati2904 <swati.lpu17@gmail.com>
2021-06-28 23:25:40 +0530

    createStore, getState, dispatch, and subscribe

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import { createStore } from "redux";
 
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-    case "ZERO":
-      return 0;
-    default:
-      // if none of the above matches, code comes here
-      return state;
-  }
-};
-
-const store = createStore(counterReducer);
-console.log(store.getState());
-store.dispatch({ type: "INCREMENT" });
-store.dispatch({ type: "INCREMENT" });
-store.dispatch({ type: "INCREMENT" });
-console.log(store.getState());
-store.dispatch({ type: "ZERO" });
-store.dispatch({ type: "DECREMENT" });
-console.log(store.getState());
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      hello world!!!
+    </div>
+  );
 }
 
 export default App;
