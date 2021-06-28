9dd55d358a689a355456ae924b2e4c9750b69e18
swati2904 <swati.lpu17@gmail.com>
2021-06-28 21:25:20 +0530

    reducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { createStore } from "redux";
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    case "ZERO":
+      return 0;
+    default:
+      // if none of the above matches, code comes here
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+console.log(store.getState());
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "INCREMENT" });
+console.log(store.getState());
+store.dispatch({ type: "ZERO" });
+store.dispatch({ type: "DECREMENT" });
+console.log(store.getState());
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
