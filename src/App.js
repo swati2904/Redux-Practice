f19a3b45c9a7fa62e02ca5db84cb0facb098cf0b
swati2904 <swati.lpu17@gmail.com>
2021-07-03 17:10:26 +0530

    redux notes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
+import React from "react";
 
-
-function App() {
-  return (
-    <div>
-      hello world!!!
-    </div>
-  );
-}
+const App = () => {
+  return <div>hello world</div>;
+};
 
 export default App;
